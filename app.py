4b3e4f09f309ff4ed5932bd9ff28e201a84ff83d
asrafull3003 <asrafull3003@gmail.com>
2024-08-26 00:07:45 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-import flask
-app = flask.Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return """
-<center> 
-    <img src="https://i.giphy.com/media/3o7abAHdYvZdBNnGZq/giphy.webp" style="border-radius: 12px;"/> 
-</center> 
-<style>
-    body { 
-        background: antiquewhite;
-    }
-</style>"""
+def hello_world():
+    return 'Hello from Tech VJ'
+
+
+if __name__ == "__main__":
+    app.run()
